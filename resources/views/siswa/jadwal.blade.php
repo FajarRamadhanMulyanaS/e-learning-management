95c91cfb424dd9df118a7d9e79db0d7ebc38a09f
fajar0411 <dhefajar0410@gmail.com>
2025-10-25 22:51:00 +0700

    done1

--- resources/views/siswa/jadwal.blade.php
+++ resources/views/siswa/jadwal.blade.php
@@ -1,74 +1,79 @@
 @extends('layout_siswa.app')
 @section('konten')
+    <style>
+        .container-fluid {
+            background-color: white;
 
-<style>
-    .container-fluid {
-        background-color: white;
+        }
 
-    }
+        h1 {
+            font-family: Times, sans-serif;
+            margin-left: 60px;
+            margin-top: 20px
+        }
 
-    h1 {
-        font-family: Times, sans-serif;
-        margin-left: 60px;
-        margin-top:  20px
-    }
+        .content {
+            margin-left: 60px;
 
-    .content {
-        margin-left: 60px;
-
-    }
-</style>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>JADWAL MATA PELAJARAN</h1>
-            <div class="content">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{route('siswa.index')}}">Home</a></li>
-                    <li class="breadcrumb-item"><span class="no-link">My courses</span></li>
-                    <li class="breadcrumb-item"><a href="#" >JADWAL MATA PELAJARAN</a></li>
-                </ol>
+        }
+    </style>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>JADWAL MATA PELAJARAN</h1>
+                <div class="content">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('siswa.index') }}">Home</a></li>
+                        <li class="breadcrumb-item"><span class="no-link">My courses</span></li>
+                        <li class="breadcrumb-item"><a href="#">JADWAL MATA PELAJARAN</a></li>
+                    </ol>
+                </div>
             </div>
         </div>
     </div>
-</div>
     <br>
-        <div class="card">
-          <div class="card-header">
-            <span class="h5 my-2"><i class="fa-solid fa-list"></i> Jadwal Mengajar Kelas </span>
-          </div>
-          <div class="card-body">
+    <div class="card">
+       <div class="card-header bg-primary text-white text-center">
+    <h5 class="mb-0">
+        <i class="fa-solid fa-list"></i> Jadwal Kelas 
+    <strong>{{ $siswa->kelas->nama_kelas ?? '-' }}</strong>
+
+    </h5>
+</div>
+
+        <div class="card-body">
             <table class="table table-hover" id="datatablesSimple">
-              <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th scope="col">Hari</th>
-                  <th scope="col">Jam</th>
-                  <th scope="col">Mapel</th>
-                  <th scope="col">Guru</th>
-                  <th scope="col">Ruang</th>
-                </tr>
-              </thead>
-              <tbody>
-              @forelse(($jadwal ?? collect()) as $index => $item)
-                <tr>
-                  <td>{{ $index + 1 }}</td>
-                  <td>{{ $item->hari ?? '-' }}</td>
-                  <td>
-                    {{ optional(\Illuminate\Support\Carbon::parse($item->jam_mulai ?? '00:00'))->format('H:i') }} -
-                    {{ optional(\Illuminate\Support\Carbon::parse($item->jam_selesai ?? '00:00'))->format('H:i') }}
-                  </td>
-                  <td>{{ optional($item->mapel)->nama_mapel ?? '-' }}</td>
-                  <td>{{ optional($item->user)->username ?? '-' }}</td>
-                  <td>{{ $item->ruang ?? '-' }}</td>
-                </tr>
-              @empty
-                <tr>
-                  <td colspan="6" class="text-center">Belum ada jadwal untuk ditampilkan.</td>
-                </tr>
-              @endforelse
-              </tbody>
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Hari</th>
+                        <th scope="col">Jam</th>
+                        <th scope="col">Mapel</th>
+                        <th scope="col">Guru</th>
+                        <th scope="col">Ruang</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse(($jadwal ?? collect()) as $index => $item)
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $item->hari ?? '-' }}</td>
+                            <td>
+                                {{ optional(\Illuminate\Support\Carbon::parse($item->jam_mulai ?? '00:00'))->format('H:i') }}
+                                -
+                                {{ optional(\Illuminate\Support\Carbon::parse($item->jam_selesai ?? '00:00'))->format('H:i') }}
+                            </td>
+                            <td>{{ optional($item->mapel)->nama_mapel ?? '-' }}</td>
+                            <td>{{ optional($item->user)->username ?? '-' }}</td>
+                            <td>{{ $item->ruang ?? '-' }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="6" class="text-center">Belum ada jadwal untuk ditampilkan.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
             </table>
-          </div>
         </div>
-    @endsection
+    </div>
+@endsection
